b389d51aea5aa1669f2ac7b6e2dd6bb754251901
miladn <milad.n91@gmail.com>
2025-03-16 20:49:28 +0330

    add responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import TabsComponent from "./TabsComponent";
 function App() {
   const tabs = [
     {
-      title: "TAB 1",
-      content: "This is the content for Tab 1",
+      title: "Tab 1",
+      content: "This is the content for Tab 1 ✨ ",
     },
     {
-      title: "TAB 2",
-      content: "This is the content for Tab 2",
+      title: "Tab 2",
+      content: "This is the content for Tab 2 😍",
     },
-    { title: "TAB 3", content: "This is the content for Tab 3" },
+    { title: "Tab 3", content: "This is the content for Tab 3 ⭐" },
     {
-      title: "TAB 4",
-      content: "This is the content for Tab 4",
+      title: "Tab 4",
+      content: "This is the content for Tab 4 👌",
     },
   ];
 
